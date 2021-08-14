824cb271dc0820abc32f966137d066a72dcbb605
Nikich37 <nikita.maculevich37@gmail.com>
2021-08-14 11:55:45 +0300

    feature

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +9,136 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  isHomeActive: boolean = false;
+  isServicesActive: boolean = false;
+  isOurTeamActive: boolean = false;
+  isContactUsActive: boolean = false;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.isHomeActive = true;
+  }
+
+  @HostListener('document:scroll', ['$event'])
+  pageScrolled() {
+    if (window.pageYOffset < 500) {
+      this.isHomeActive = true;
+      this.isServicesActive = false;
+      this.isOurTeamActive = false;
+      this.isContactUsActive = false;
+    }
+    if (window.innerWidth > 1376) {
+      if (window.pageYOffset > 500 && window.pageYOffset < 1700) {
+        this.isHomeActive = false;
+        this.isServicesActive = true;
+        this.isOurTeamActive = false;
+        this.isContactUsActive = false;
+      }
+      if (window.pageYOffset >= 1700 && window.pageYOffset < 2600) {
+        this.isHomeActive = false;
+        this.isServicesActive = false;
+        this.isOurTeamActive = true;
+        this.isContactUsActive = false;
+      }
+      if (window.pageYOffset >= 2600) {
+        this.isHomeActive = false;
+        this.isServicesActive = false;
+        this.isOurTeamActive = false;
+        this.isContactUsActive = true;
+      }
+    } else if (window.innerWidth <= 1376 && window.innerWidth > 1009) {
+      if (window.pageYOffset > 500 && window.pageYOffset < 2070) {
+        this.isHomeActive = false;
+        this.isServicesActive = true;
+        this.isOurTeamActive = false;
+        this.isContactUsActive = false;
+      }
+      if (window.pageYOffset >= 2070 && window.pageYOffset < 3400) {
+        this.isHomeActive = false;
+        this.isServicesActive = false;
+        this.isOurTeamActive = true;
+        this.isContactUsActive = false;
+      }
+      if (window.pageYOffset >= 3400) {
+        this.isHomeActive = false;
+        this.isServicesActive = false;
+        this.isOurTeamActive = false;
+        this.isContactUsActive = true;
+      }
+    } else if (window.innerWidth >= 509 && window.innerWidth < 1009) {
+      if (window.pageYOffset > 500 && window.pageYOffset < 2070) {
+        this.isHomeActive = false;
+        this.isServicesActive = true;
+        this.isOurTeamActive = false;
+        this.isContactUsActive = false;
+      }
+      if (window.pageYOffset >= 2070 && window.pageYOffset < 4300) {
+        this.isHomeActive = false;
+        this.isServicesActive = false;
+        this.isOurTeamActive = true;
+        this.isContactUsActive = false;
+      }
+      if (window.pageYOffset >= 4300) {
+        this.isHomeActive = false;
+        this.isServicesActive = false;
+        this.isOurTeamActive = false;
+        this.isContactUsActive = true;
+      }
+    } else if (window.innerWidth <= 510) {
+      if (window.pageYOffset > 500 && window.pageYOffset < 2070) {
+        this.isHomeActive = false;
+        this.isServicesActive = true;
+        this.isOurTeamActive = false;
+        this.isContactUsActive = false;
+      }
+      if (window.pageYOffset >= 3350 && window.pageYOffset < 5000) {
+        this.isHomeActive = false;
+        this.isServicesActive = false;
+        this.isOurTeamActive = true;
+        this.isContactUsActive = false;
+      }
+      if (window.pageYOffset >= 5000) {
+        this.isHomeActive = false;
+        this.isServicesActive = false;
+        this.isOurTeamActive = false;
+        this.isContactUsActive = true;
+      }
+    }
+  }
+
+  homeClick() {
+
+    window.scrollTo(0, 0);
+  }
+
+  servicesClick() {
+    window.scrollTo(0, 950);
+  }
+
+  ourTeamClick() {
+    if (window.innerWidth > 1376) {
+      window.scrollTo(0, 2050);
+    } else if (window.innerWidth <= 510) {
+      window.scrollTo(0, 3700);
+    } else if (window.innerWidth <= 1376) {
+      window.scrollTo(0, 2420);
+    }
+  }
+
+  contactUsClick() {
+    if (window.innerWidth > 1505) {
+      window.scrollTo(0, 3000);
+    } else if (window.innerWidth <= 460) {
+      window.scrollTo(0, 5600);
+    } else if (window.innerWidth <= 1009) {
+      window.scrollTo(0, 4500);
+    } else if (window.innerWidth <= 1377) {
+      window.scrollTo(0, 3900);
+    } else if (window.innerWidth <= 1505) {
+      window.scrollTo(0, 3500);
+    }
   }
 
 }
